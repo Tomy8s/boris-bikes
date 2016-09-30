16b3ad1e736ae82dd3c0c4d01201adaad4eaaf04
Tomy8s <tomy8s@hotmail.com>
2016-09-30 12:27:32 +0100

    test for van.deliver_broken

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,20 +5,24 @@ require 'van'
 
 describe Van do
 
-# Needs to pick up broken bikes from docking station
+  # Needs to pick up broken bikes from docking station
 
-it 'picks up broken bikes' do
-  bike = Bike.new
-  bike.report_broken
-  station = DockingStation.new
-  station.dock(bike)
-  subject.pick_up(station)
-  expect(subject.cargo).to eq [bike]
-end
+  it 'picks up broken bikes' do
+    bike = Bike.new
+    bike.report_broken
+    station = DockingStation.new
+    station.dock(bike)
+    subject.pick_up(station)
+    expect(subject.broken_bike).to eq [bike]
+  end
 
-# Needs to deliver broken bikes to garage
+  # Needs to deliver broken bikes to garage
 
-
-
+  it 'delivers broken bikes to garage' do
+    subject.broken_bike = [double(:bike, broken?: true)]
+    garage = Garage.new
+    subject.deliver_broken(garage)
+    expect(garage.broken_bike).to eq [bike]
+  end
 
 end
